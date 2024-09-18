3dedf90f78c5bee1e3d34507a638623f475a1f5e
Azrian <noman89reg@gmail.com>
2024-09-18 13:16:34 +0300

    stat_svc_dto

--- stats/stats-dto/src/main/java/dto/StatsRequestDTO.java
+++ stats/stats-dto/src/main/java/dto/StatsRequestDTO.java
@@ -1,7 +1,6 @@
 package dto;
 
 import lombok.Data;
-import validation.Create;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
@@ -9,10 +8,10 @@ import java.util.List;
 @Data
 public class StatsRequestDTO {
 
-    @NotNull(groups = Create.class)
+    @NotNull
     private String start;
 
-    @NotNull(groups = Create.class)
+    @NotNull
     private String end;
 
     private List<String> uris;
