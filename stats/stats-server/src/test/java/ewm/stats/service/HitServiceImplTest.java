0df34732b307ce9bfd831940964c9b918f1b0077
ton1p <posohin.tosha@yandex.ru>
2024-09-20 17:36:26 +0300

    fix: application.properties and tests

--- stats/stats-server/src/test/java/ewm/stats/service/HitServiceImplTest.java
+++ stats/stats-server/src/test/java/ewm/stats/service/HitServiceImplTest.java
@@ -2,16 +2,18 @@ package ewm.stats.service;
 
 import ewm.dto.EndpointHitDTO;
 import ewm.dto.EndpointHitResponseDto;
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
+@Transactional
 @SpringBootTest
-@AllArgsConstructor(onConstructor_ = {@Autowired})
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 class HitServiceImplTest {
-    private HitService hitService;
+    private final HitService hitService;
 
     private static final EndpointHitDTO endpointHitDto = EndpointHitDTO.builder()
             .timestamp("2024-09-20 00:00:00")
