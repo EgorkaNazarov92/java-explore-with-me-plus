041524b4dcf0dcd745f4801d867b4a83b7baf9ca
ton1p <posohin.tosha@yandex.ru>
2024-09-20 14:49:01 +0300

    feat: add server module

--- stats/stats-client/src/main/java/ewm/stats/StatsClient.java
+++ stats/stats-client/src/main/java/ewm/stats/StatsClient.java
@@ -1,8 +1,8 @@
 package ewm.stats;
 
-import dto.EndpointHitDTO;
-import dto.StatsRequestDTO;
 import ewm.client.BaseClient;
+import ewm.dto.EndpointHitDTO;
+import ewm.dto.StatsRequestDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -14,21 +14,21 @@ import org.springframework.web.util.DefaultUriBuilderFactory;
 @Service
 public class StatsClient extends BaseClient {
 
-	@Autowired
-	public StatsClient(@Value("${server.url}") String serverUrl, RestTemplateBuilder builder) {
-		super(
-				builder
-						.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-						.requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
-						.build()
-		);
-	}
+    @Autowired
+    public StatsClient(@Value("${server.url}") String serverUrl, RestTemplateBuilder builder) {
+        super(
+                builder
+                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
+                        .requestFactory(() -> new HttpComponentsClientHttpRequestFactory())
+                        .build()
+        );
+    }
 
-	public ResponseEntity<Object> saveHit(EndpointHitDTO requestDto) {
-		return post("/hit", requestDto);
-	}
+    public ResponseEntity<Object> saveHit(EndpointHitDTO requestDto) {
+        return post("/hit", requestDto);
+    }
 
-	public ResponseEntity<Object> getStats(StatsRequestDTO headersDto) {
-		return get("/stats", headersDto);
-	}
+    public ResponseEntity<Object> getStats(StatsRequestDTO headersDto) {
+        return get("/stats", headersDto);
+    }
 }
---
+++ stats/stats-server/src/test/java/ewm/stats/service/HitServiceImplTest.java
@@ -0,0 +1,58 @@
+package ewm.stats.service;
+
+import ewm.dto.EndpointHitDTO;
+import ewm.dto.EndpointHitResponseDto;
+import ewm.stats.model.Hit;
+import ewm.stats.repository.HitRepository;
+import lombok.AllArgsConstructor;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@AllArgsConstructor(onConstructor_ = {@Autowired})
+class HitServiceImplTest {
+    @MockBean
+    private HitRepository hitRepository;
+
+    @Autowired
+    private HitService hitService;
+
+    private static final Hit hit = Hit.builder()
+            .id(1L)
+            .ip("test")
+            .timestamp(LocalDateTime.of(2024, 9, 20, 0, 0, 0))
+            .app("test")
+            .uri("test")
+            .build();
+
+    private static final EndpointHitDTO endpointHitDto = EndpointHitDTO.builder()
+            .timestamp("2024-09-20 00:00:00")
+            .app("test")
+            .uri("test")
+            .build();
+
+    private static final EndpointHitResponseDto endpointHitResponseDto = EndpointHitResponseDto.builder()
+            .id(1L)
+            .timestamp("2024-09-20 00:00:00")
+            .app("test")
+            .uri("test")
+            .ip("test")
+            .build();
+
+    @Test
+    void create() {
+        Mockito.when(hitRepository.save(Mockito.any())).thenReturn(hit);
+        EndpointHitResponseDto responseDto = hitService.create(endpointHitDto);
+        Assertions.assertNotNull(responseDto);
+        Assertions.assertEquals(endpointHitResponseDto.getApp(), responseDto.getApp());
+        Assertions.assertEquals(endpointHitResponseDto.getUri(), responseDto.getUri());
+        Assertions.assertEquals(endpointHitResponseDto.getIp(), responseDto.getIp());
+        Assertions.assertEquals(endpointHitResponseDto.getId(), responseDto.getId());
+    }
+}
